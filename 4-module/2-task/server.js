8b37770cc1aebf35e3cf01dabd2960c6b0d01e39
Vladimir Lapchenko <vlr.lapchenko@gmail.com>
2020-04-27 17:54:32 +0300

    add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,22 +1,60 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const LimitSizeStream = require("./LimitSizeStream");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'POST':
+    case "POST": {
+      if (pathname.includes("/") || pathname.includes("..")) {
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
+
+      const limitSizeStream = new LimitSizeStream({ limit: 1024 * 1024 });
+      const file = fs.createWriteStream(filepath, { flags: "wx" });
+
+      limitSizeStream.on("error", (error) => {
+        console.log(error.message);
+        res.statusCode = 413;
+        res.end();
+        file.end();
+      });
+
+      file.on("error", (error) => {
+        res.statusCode = 409;
+        res.end();
+      });
+
+      file.on("finish", () => {
+        if (res.finished) return;
+        console.log("file finished");
+        res.statusCode = 201;
+        res.end();
+      });
+
+      res.on("close", () => {
+        if (res.finished) return;
+        limitSizeStream.destroy();
+        file.destroy();
+        fs.unlinkSync(filepath);
+      });
+
+      req.pipe(limitSizeStream).pipe(file);
 
       break;
+    }
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
