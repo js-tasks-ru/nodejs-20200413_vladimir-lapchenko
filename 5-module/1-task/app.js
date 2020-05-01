751fea0fe0b858402d54d94dc985826f4bcdf94a
Vladimir Lapchenko <vlr.lapchenko@gmail.com>
2020-05-01 12:01:55 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,17 +1,33 @@
-const path = require('path');
-const Koa = require('koa');
+const path = require("path");
+const Koa = require("koa");
+const EventEmitter = require("events");
+
 const app = new Koa();
+const ee = new EventEmitter();
 
-app.use(require('koa-static')(path.join(__dirname, 'public')));
-app.use(require('koa-bodyparser')());
+app.use(require("koa-static")(path.join(__dirname, "public")));
+app.use(require("koa-bodyparser")());
 
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get("/subscribe", async (ctx, next) => {
+  const promise = new Promise((resolve) => {
+    ee.on("message", (msg) => {
+      resolve(msg);
+    });
+  });
+  ctx.response.body = await promise;
+  return next();
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post("/publish", async (ctx, next) => {
+  const { message } = ctx.request.body;
+  if (message) {
+    ee.emit("message", message);
+  }
+  ctx.response.body = "";
+  return next();
 });
 
 app.use(router.routes());
