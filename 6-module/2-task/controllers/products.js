7f24c0c602a8a4c76a16f257ee0000f788b36e67
Vladimir Lapchenko <vlr.lapchenko@gmail.com>
2020-05-11 12:53:57 +0300

    add solution for 6-module 3-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -7,12 +7,8 @@ module.exports.productsBySubcategory = async function productsBySubcategory(
   next
 ) {
   const { subcategory } = ctx.request.query;
-  let dbRecords = [];
-  if (subcategory) {
-    dbRecords = await Product.find({ subcategory });
-  } else {
-    dbRecords = await Product.find({});
-  }
+  if (!subcategory) return next();
+  const dbRecords = await Product.find({ subcategory });
   const products = dbRecords.map((p) => p.map());
   ctx.body = { products };
 };
@@ -21,7 +17,6 @@ module.exports.productList = async function productList(ctx, next) {
   const dbRecords = await Product.find({});
   const products = dbRecords.map((r) => r.map());
   ctx.body = { products };
-  return next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,13 @@
+const Product = require("../models/Product");
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query } = ctx.query;
+  const dbRecords = await Product.find(
+    {
+      $text: { $search: query },
+    },
+    { score: { $meta: "textScore" } }
+  ).sort({ score: { $meta: "textScore" } });
+  const products = dbRecords.map((r) => r.map());
+  ctx.body = { products };
 };
